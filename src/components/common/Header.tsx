935c59a9d55c57b0acac41458393a6faf3fdb95c
相原 直也 <aiharanaoya@team-lab.com>
2022-08-31 20:38:53 +0900

    feat: LP表示時はヘッダーを戻るアイコンに切り替える

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,13 +1,27 @@
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 import { FC } from 'react';
+import IconBack from '@/assets/icons/back.svg';
 import style from './Header.module.scss';
 
 export const Header: FC = () => {
+  const router = useRouter();
+
+  if (router.pathname === '/') {
+    return (
+      <header className={style['home-header']}>
+        <Link href="/">
+          <a className={style['header-link']}>LP Collection</a>
+        </Link>
+      </header>
+    );
+  }
+
   return (
-    <header className={style['common-header']}>
-      <Link href="/">
-        <a className={style['header-link']}>LP Collection</a>
-      </Link>
-    </header>
+    <Link href="/">
+      <div className={style['icon-link']}>
+        <IconBack />
+      </div>
+    </Link>
   );
 };
